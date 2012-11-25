c7520b9874f1d1521fccf250105bc6e7acd6f87a
Delwin Campbell <d@delw.in>
2012-11-25 15:52:30 -0600

    getting closer...

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, make_response
+import urllib2, simplejson
+from urllib import urlencode
 
 DEBUG = True
 app = Flask(__name__)
@@ -17,20 +19,40 @@ class Weather:
 	def colder_than(self, other):
 		return (self.avg < other.avg - 4)
 
+def standardize_description(description):
+	# based on codes from 
+	# http://www.worldweatheronline.com/feed/wwoConditionCodes.txt
 
-def celsius(fahrenheit):
-	return (fahrenheit - 32) * 5/9
+	# compare description
+		# if contains 'clear' or 'sunny', return 'clear'
+		# if contains 'cloudy', 'overcast', 'fog', return 'clouds'
+		# if contains 'thunder', return 'storm'
+		# if contains 'ice', 'sleet', 'freezing rain', 'freezing drizzle'
+		# if contains 'snow', 'blizzard', 
+		# if contains 'rain', 'drizzle', 'mist', return 'wet'
+	return 0
+
+def get_json(url):
+	req = urllib2.Request(url)
+	opener = urllib2.build_opener()
+	f = opener.open(req)
+	return simplejson.load(f)
 
 def yesterdays_weather(location):
+	weather_json = get_json('http://autocomplete.wunderground.com/aq?query=' + location)
+
 	high = 55
 	low = 44
 	conditions = 'clear'
 	return Weather(high, low, conditions)
 
 def todays_weather(location):
-	high = 33
-	low = 22
-	conditions = 'snow'
+	weather_json = get_json("http://free.worldweatheronline.com/feed/weather.ashx?q=" + location + "&format=json&key=6f0b244099202239122511")
+
+	high = int(weather_json['data']['weather'][0]['tempMaxF'])
+	low = int(weather_json['data']['weather'][0]['tempMinF'])
+	# conditions = get_conditions_from_code(weather_json['data']['weather'][0]['weatherCode'])
+	conditions = weather_json['data']['weather'][0]['weatherDesc'][0]['value']
 	return Weather(high, low, conditions)
 
 def compare(location):
@@ -76,6 +98,8 @@ def index(zipcode=None):
 	if (zipcode != default_zipcode()):
 		new_zipcode = request.args.get('zipcode')
 		resp = make_response(render_template('index.html', 
+			today=todays_weather(zipcode),
+			yesterday=yesterdays_weather(zipcode),
 			weather=compare(new_zipcode), 
 			zipcode=new_zipcode))
 		resp.set_cookie('zipcode', new_zipcode)
