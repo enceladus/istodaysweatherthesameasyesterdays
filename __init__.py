66ae80f85b6c2687aca853aa08e4b6194c374d0b
Delwin Campbell <d@delw.in>
2013-06-10 16:26:13 -0500

    stuff

--- __init__.py
+++
@@ -1,6 +0,0 @@
-class WebFactionMiddleware(object):
-    def __init__(self, app):
-        self.app = app
-    def __call__(self, environ, start_response):
-        environ['SCRIPT_NAME'] = '/istodaysweatherthesameasyesterdays'
-        return self.app(environ, start_response)
--- application.py
+++ application.py
@@ -13,6 +13,14 @@ import keys
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+class WebFactionMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = '/istodaysweatherthesameasyesterdays'
+        return self.app(environ, start_response)
+
 app.wsgi_app = WebFactionMiddleware(app.wsgi_app)
 
 # database config
